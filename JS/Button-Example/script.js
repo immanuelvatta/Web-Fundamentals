535f9413eb433607c69fb5a62c618451d0668d29
immanuelvatta <immanuelvatta@gmail.com>
2023-06-17 02:07:55 -0500

    added a button example

---
+++ JS/Button-Example/script.js
@@ -0,0 +1,8 @@
+// function displayName(elementName){
+//     console.log(elementName);
+// }
+
+function clicked(element){
+    console.log("I've been clicked", element);
+    element.innerText = "Goodbye";
+}
