920be403ea3efb006f8edcd37f782d01b76a0184
immanuelvatta <immanuelvatta@gmail.com>
2023-06-18 00:06:59 -0500

    added functionality to button clicker

---
+++ JS/ButtonClicker/script.js
@@ -0,0 +1,13 @@
+function sessionStatus(status){
+    if(status.innerText=="Login"){
+        status.innerText="Logout";
+    }
+}
+
+function alertLikes(){
+    alert("Ninja was liked");
+}
+
+function removeButton(button){
+    button.remove();
+}
