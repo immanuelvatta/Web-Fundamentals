95337b31f1925ba030ed00e6c90a818cbbaf911d
immanuelvatta <immanuelvatta@gmail.com>
2023-06-18 00:19:12 -0500

    preview video using onmouseover and onmouseout

---
+++ JS/VideoPreview/script.js
@@ -0,0 +1,14 @@
+console.log("page loaded...");
+
+function playVideo(elements){
+    elements.play();
+    elements.muted = true;
+
+
+}
+
+function pauseVideo(elements){
+    elements.pause();
+}
+
+
